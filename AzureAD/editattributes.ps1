ad1accc7e03a862394d14ec80da313a588f5bb4b
Pedro Alejandro Blanco <53581574+pblancodev@users.noreply.github.com>
2023-06-10 16:16:35 +0100

    Update editattributes.ps1

--- AzureAD/editattributes.ps1
+++ AzureAD/editattributes.ps1
@@ -1,4 +1,4 @@
-﻿#Paso 1
+#Paso 1
 Install-Module AzureAD
 
 #Paso 2
@@ -7,5 +7,14 @@ Import-Module AzureAD
 #Paso 3
 Connect-AzureAD
 
-#Paso 4
-Update-AzADUser -UPNOrObjectId  pedro.blanco@prueba.local -City "Valencia"  -JobTitle "Ingeniero" -Department "Informàtica" -OfficeLocation "Av Bolivar 234"
+#Paso 5 Ruta del archivo CSV
+$csvPath = "C:\archivo.csv"
+
+#Paso 6 Leer los correos desde el archivo CSV
+$correos = Import-Csv -Path $csvPath | Select-Object -ExpandProperty Correo
+
+#Paso 7 Recorrer cada correo y aplicar los cambios
+foreach ($correo in $correos) {
+    
+    $usuario = Get-AzureADUser -UPNOrObjectId $correo  -CompanyName "UnicomCorp" -Country "Venezuela" -Country "Finanzas "
+}
