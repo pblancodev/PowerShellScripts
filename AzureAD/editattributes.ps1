37a31a85071fcbbda00e1a6d0816fca1f6347b20
Pedro Alejandro Blanco <53581574+pblancodev@users.noreply.github.com>
2023-06-10 16:06:29 +0100

    v1

---
+++ AzureAD/editattributes.ps1
@@ -0,0 +1,11 @@
+﻿#Paso 1
+Install-Module AzureAD
+
+#Paso 2
+Import-Module AzureAD
+
+#Paso 3
+Connect-AzureAD
+
+#Paso 4
+Update-AzADUser -UPNOrObjectId  pedro.blanco@prueba.local -City "Valencia"  -JobTitle "Ingeniero" -Department "Informàtica" -OfficeLocation "Av Bolivar 234"
