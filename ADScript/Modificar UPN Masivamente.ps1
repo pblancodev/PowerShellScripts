f90fb38e7ff9318c6947274b8b3fb43bae82feb0
Pedro Alejandro Blanco <53581574+pblancodev@users.noreply.github.com>
2023-08-11 11:59:57 +0100

    Create Modificar UPN Masivamente.ps1

---
+++ ADScript/Modificar UPN Masivamente.ps1
@@ -0,0 +1,6 @@
+﻿$Domain = 'agros.pt'
+$User = 'Manuel Ferreira'
+Get-ADUser $User | select Name, UserPrincipalName
+
+Get-ADUser $User | Set-ADUser -UserPrincipalName "$user@$domain"
+Get-ADUser $User | select Name, UserPrincipalName
