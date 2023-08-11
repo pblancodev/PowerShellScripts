d5caeaabb37aeed10da0c6f8479c5a140a1a16d7
Pedro Alejandro Blanco <53581574+pblancodev@users.noreply.github.com>
2023-08-11 17:19:24 +0100

    Update Modificar UPN Masivamente.ps1

--- ADScript/Modificar UPN Masivamente.ps1
+++ ADScript/Modificar UPN Masivamente.ps1
@@ -4,3 +4,23 @@ Get-ADUser $User | select Name, UserPrincipalName
 
 Get-ADUser $User | Set-ADUser -UserPrincipalName "$user@$domain"
 Get-ADUser $User | select Name, UserPrincipalName
+
+
+################# multi user #################################
+
+
+$Domain = 'prodistica.pt'
+
+$CSVPath ="C:\Users\RIS2048\Documents\userupn\users.csv"
+
+$Users = Import-Csv -Path $CSVPath
+
+foreach ($User in $Users) {
+    $Username = $User.Users
+
+    Get-ADUser $Username | Select-Object Name, UserPrincipalName
+
+    Set-ADUser -Identity $Username -UserPrincipalName "$Username@$Domain"
+
+    Get-ADUser $Username | Select-Object Name, UserPrincipalName
+}
